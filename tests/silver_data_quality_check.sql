cf9d6acbc3e0103ff17691858309f153ab4971eb
shubhamparulekar <33445599+shubhamparulekar@users.noreply.github.com>
2025-07-12 19:49:28 -0500

    Update silver_data_quality_check.sql

--- tests/silver_data_quality_check.sql
+++ tests/silver_data_quality_check.sql
@@ -1,12 +1,7 @@
 /*
 
     This script performs various quality checks for data consistency, accuracy, 
-    and standardization across the 'silver' layer. It includes checks for:
-    - Null or duplicate primary keys.
-    - Unwanted spaces in string fields.
-    - Data standardization and consistency.
-    - Invalid date ranges and orders.
-    - Data consistency between related fields
+    and standardization across the 'silver' layer. 
 */
 
 -- Checking 'silver.crm_cust_info'
